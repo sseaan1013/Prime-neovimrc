7f4ee09677dfee0e08fc24de8346e63a003d9e54
Sean Phelan <sean.phelan1013@gmail.com>
2026-03-04 11:02:50 -0500

    Add filepath dropdown source to LSP configuration

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -75,6 +75,7 @@ return {
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
                 { name = 'luasnip' }, -- For luasnip users.
+                { name = 'path' }, --filepath dropdown
             }, {
                 { name = 'buffer' },
             })
